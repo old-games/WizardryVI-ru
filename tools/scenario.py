b5c4228e92035706e7cbf7f0afad65e34d1a2c1e
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2025-09-09 11:27:00 +0300

    Separated blocks in `SCENARIO.DBS`.

--- tools/scenario.py
+++ tools/scenario.py
@@ -1,4 +1,14 @@
-def decode(data: bytes, disk: bytes) -> list[bytes]:
+import enum
+
+
+class ScenarioBlock(enum.Enum):
+    EXPERIENCE = 0
+    ITEM = 1
+    MONSTER = 4
+    NPC = 5
+
+
+def decode(data: bytes, disk: bytes) -> list[bytes | list[bytes]]:
     assert len(disk) >= 0x2c
     assert disk[:4] == b'\x00'*4
     offsets = [int.from_bytes(disk[i:i+4], 'little') for i in range(4, 0x2c, 4)]
@@ -11,6 +21,20 @@ def decode(data: bytes, disk: bytes) -> list[bytes]:
         start = offsets[i]
         end = offsets[i+1]
         results.append(data[start:end])
+    # FIXME move it to corresponding module
+    assert len(results[ScenarioBlock.EXPERIENCE.value]) == 0x0e * 4 * 16, 'EXPERIENCE block size must be 0x0e * 4 * 16.'
+    assert len(results[ScenarioBlock.ITEM.value]) % 0x4a == 0, 'ITEM block size must be multiple of 0x4a.'
+    assert len(results[ScenarioBlock.MONSTER.value]) % 0xde == 0, 'MONSTER block size must be multiple of 0xde.'
+    assert len(results[ScenarioBlock.NPC.value]) % 0x8e == 0, 'NPC block size must be multiple of 0x8e.'
+    for block, record_size in [
+        (ScenarioBlock.EXPERIENCE, 0x40),
+        (ScenarioBlock.ITEM, 0x4a),
+        (ScenarioBlock.MONSTER, 0xde),
+        (ScenarioBlock.NPC, 0x8e),
+    ]:
+        block_data = results[block.value]
+        records = [block_data[i:i+record_size] for i in range(0, len(block_data), record_size)]
+        results[block.value] = records
     return results
 
 
@@ -26,5 +50,13 @@ if __name__ == '__main__':
     scenarios = decode(scenario_data, disk_data)
     os.makedirs('unpacked', exist_ok=True)
     for i, scenario in enumerate(scenarios):
-        with open(f'unpacked/SCENARIO.{i}.DBS', 'wb') as f:
-            f.write(scenario)
+        if isinstance(scenario, bytes):
+            with open(f'unpacked/SCENARIO.{i}.DBS', 'wb') as f:
+                f.write(scenario)
+        else:
+            records_size = len(scenario)
+            digits = len(str(records_size-1))
+            for j, record in enumerate(scenario):
+                j = str(j).rjust(digits, '0')
+                with open(f'unpacked/SCENARIO.{i}.{j}.DBS', 'wb') as f:
+                    f.write(record)
